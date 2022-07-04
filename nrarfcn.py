58f2d8081afd39386cc7bbc8fb6e5b97be8ec83e
brenolevi <brenolevi@ymail.com>
2022-07-04 20:40:46 -0300

    Create Table class

--- nrarfcn.py
+++ nrarfcn.py
@@ -1,3 +1,13 @@
+class Table:
+    def __init__(self, name: str, header: list, data: list):
+        self.name = name
+        self.header = header
+        self.data = data
+
+    def get_cell(self, row: list, key: str):
+        idx = self.header.index(key)
+        return row[idx]
+
 def get_frequency(nrarfcn: int) -> float:
     """Gets the frequency of a given NR-ARFCN, in MHz.
 
@@ -11,45 +21,26 @@ def get_frequency(nrarfcn: int) -> float:
         ValueError: If the NR-ARFCN is not valid.
     """
 
-    freq_nr_table = [
-        {
-            'F_min_MHz': 0,
-            'F_max_MHz': 3000,
-            'DF_global_kHz': 5,
-            'Fref_offs_MHz': 0,
-            'Nref_offs': 0,
-            'Nref_min': 0,
-            'Nref_max': 599999
-        },
-        {
-            'F_min_MHz': 3000,
-            'F_max_MHz': 24250,
-            'DF_global_kHz': 15,
-            'Fref_offs_MHz': 3000,
-            'Nref_offs': 600000,
-            'Nref_min': 600000,
-            'Nref_max': 2016666
-        },
-        {
-            'F_min_MHz': 24250,
-            'F_max_MHz': 100000,
-            'DF_global_kHz': 60,
-            'Fref_offs_MHz': 24250.08,
-            'Nref_offs': 2016667,
-            'Nref_min': 2016667,
-            'Nref_max': 3279165
-        }
+    table_name = "Table 5.4.2.1-1: NR-ARFCN parameters for the global frequency raster"
+    table_header = ['F_min_MHz', 'F_max_MHz', 'DF_global_kHz', 'Fref_offs_MHz', 'Nref_offs', 'Nref_min', 'Nref_max']
+    table_data = [
+        [0,      3_000,   5,  0,         0,         0,         599_999],
+        [3_000,  24_250,  15, 3_000,     600_000,   600_000,   2_016_666],
+        [24_250, 100_000, 60, 24_250.08, 2_016_667, 2_016_667, 3_279_165]
     ]
+
+    table = Table(table_name, table_header, table_data)
 
     if type(nrarfcn) is not int:
         raise ValueError('NR-ARFCN must be an integer.')
 
-    if nrarfcn < 0 or nrarfcn > 3279165:
-        raise ValueError('NR-ARFCN must be between 0 and 3279165.')
+    if nrarfcn < 0 or nrarfcn > 3_279_165:
+        raise ValueError('NR-ARFCN must be between 0 and 3,279,165.')
 
-    for row in freq_nr_table:
-        if row['Nref_min'] <= nrarfcn <= row['Nref_max']:
-            freq_kHz = int(row['Fref_offs_MHz']*1000) + row['DF_global_kHz'] * (nrarfcn - row['Nref_offs'])
+    for row in table.data:
+        if table.get_cell(row, 'Nref_min') <= nrarfcn <= table.get_cell(row, 'Nref_max'):
+            freq_kHz = int(table.get_cell(row, 'Fref_offs_MHz') * 1000) \
+                       + table.get_cell(row, 'DF_global_kHz') * (nrarfcn - table.get_cell(row, 'Nref_offs'))
             freq_MHz = freq_kHz / 1000
             return freq_MHz
 
