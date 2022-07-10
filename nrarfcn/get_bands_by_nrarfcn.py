7f7ae4fe937bf52b20cd80e981f8c1a6d45205ec
Breno Levi <blevic@proton.me>
2022-07-10 11:28:30 -0300

    Indirect implementation of get_bands_by_nrarfcn

---
+++ nrarfcn/get_bands_by_nrarfcn.py
@@ -0,0 +1,28 @@
+from nrarfcn.get_bands_by_frequency import get_bands_by_frequency
+from nrarfcn.get_frequency import get_frequency
+
+
+def get_bands_by_nrarfcn(nrarfcn: int) -> list:
+    """Gets the bands of a given NR-ARFCN.
+
+    Args:
+        nrarfcn: The NR-ARFCN to get the bands of.
+
+    Returns:
+        The bands of the given NR-ARFCN.
+
+    Raises:
+        ValueError: If the NR-ARFCN is not valid.
+    """
+
+    if not isinstance(nrarfcn, int):
+        raise ValueError('NR-ARFCN must be an integer.')
+
+    if nrarfcn < 0 or nrarfcn > 3_279_165:
+        raise ValueError('NR-ARFCN must be between 0 and 3,279,165.')
+
+    bands = []
+    for band in get_bands_by_frequency(get_frequency(nrarfcn)):
+        bands.append(band)
+
+    return bands
